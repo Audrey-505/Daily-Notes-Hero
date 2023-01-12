adb9a6c53543c041e44372304816982966a36db9
Audrey Alcantara <adrianaquin22@gmail.com>
2023-01-11 20:02:26 -0500

    added get request to retrieve note file

--- server.js
+++ server.js
@@ -8,10 +8,24 @@ app.use (express.urlencoded({ extended: true }))
 
 app.use(express.static('public'))
 
+const notes = require('./db/db.json')
+
+
 app.get('/notes', (req, res) => {
     res.sendFile(`${__dirname}/public/notes.html`)
     console.log(`${req.method} request made to get the notes.html page`)
 })
+
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(notes)
+    console.log(`${req.method} request made to get the notes file`)
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+    console.log(`${req.method} request made to get the index.html page`)
+})
+
 
 
 
