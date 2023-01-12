71ab713bef09c99bab4bdf6ff76bc26991450cd4
Audrey Alcantara <adrianaquin22@gmail.com>
2023-01-11 21:45:18 -0500

    added post request to add a note

--- server.js
+++ server.js
@@ -26,7 +26,33 @@ app.get('*', (req, res) => {
     console.log(`${req.method} request made to get the index.html page`)
 })
 
-
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body
+    if (title && text) {
+        const newNote = {
+            title,
+            text
+        }
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err)
+            } else {
+                const parsedNotes = JSON.parse(data)
+                parsedNotes.push(newNote)
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
+                (writeErr) => writeErr ? console.error(writeErr) : console.info ('Successfully added note to notes!'))
+            }
+        })
+        const response = {
+            status: 'success',
+            body: newNote,
+        }
+        console.log(response)
+        res.status(201).json(response)
+    } else {
+        res.status(500).json('Error adding note')
+    }
+})
 
 
 
